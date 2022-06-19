a6b4a0ce0e5966608c59f662609e122644cceca1
Junha-K <100997960+Junha-K@users.noreply.github.com>
2022-06-19 14:15:08 +0900

    Update source.js

--- source.js
+++ source.js
@@ -6,18 +6,27 @@ function FrameHandler(self){
 	}
 }
 
+const Collection1REF = ['https://hunkim.github.io/ml/','https://url.kr/px3s2o'];
+const Collection1Text = ['모두를 위한 머신러닝/딥러닝 강의','노벨연구소 선정 역대 최고의 책'];
+
 function AddorHideLink(self){
 	if(self.className === 'expanded'){
-		let a = document.createElement('a');
-		var linktext = document.createTextNode("hunkim.github.io");
-		a.title = "moving to hunkim.github.io";
-		a.href = "https://hunkim.github.io/ml/";
-		a.appendChild(linktext);
-		var element = document.getElementById('collection1');
-		element.appendChild(a);
+		for(i=0; i<Collection1REF.length; i++){
+			let a = document.createElement('a');
+			let linktext = document.createTextNode(Collection1Text[i]);
+			a.title = "moving to"+Collection1Text[i];
+			a.href = Collection1REF[i];
+			a.appendChild(linktext);
+			let alist = document.createElement('li');
+			alist.appendChild(a);
+			var element = document.getElementById('List1');
+			element.appendChild(alist);
+		}
 	}else{
-		let links = document.getElementById('collection1');
-		links.removeChild(links.lastChild)
+		for(i=0;i<Collection1REF.length;i++){
+			let links = document.getElementById('List1');
+			links.removeChild(links.lastChild)
+		}
+
 	}
 }
-
